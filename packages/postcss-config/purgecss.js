7163e3e9d78617b61e46b9466b1f4d1f96f027c2
Ben Edwards <hello@benedfit.com>
2024-12-19 17:02:34 +0000

    fix(postcss-config): switch to plugin array

--- packages/postcss-config/purgecss.js
+++ packages/postcss-config/purgecss.js
@@ -1,15 +1,16 @@
 const { plugins } = require('.')
+const { purgeCSSPlugin } = require('@fullhuman/postcss-purgecss')
 
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-  plugins: {
+  plugins: [
     ...plugins,
-    '@fullhuman/postcss-purgecss': {
+    purgeCSSPlugin({
       content: [
         './node_modules/@newhighsco/{chipset,press-start}/**/*.[j|t]s?(x)',
         './src/{components,layouts,pages}/**/*.[j|t]s?(x)'
       ],
       safelist: [/^:(global|export)/, 'html', 'body']
-    }
-  }
+    })
+  ]
 }
