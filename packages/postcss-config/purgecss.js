eff5ea38fe6a3d70f9a29d41a2b460d7618a2c60
Ben Edwards <hello@benedfit.com>
2025-08-29 17:57:52 +0100

    Revert "fix(postcss): support monorepos when purging (#2248)"
    This reverts commit c7b2de22d46e95b6e93ec2b39a61a5c777aa497a.

--- packages/postcss-config/purgecss.js
+++ packages/postcss-config/purgecss.js
@@ -6,8 +6,8 @@ module.exports = {
     ...plugins,
     '@fullhuman/postcss-purgecss': {
       content: [
-        '**/node_modules/@newhighsco/{chipset,press-start}/**/*.[j|t]s?(x)',
-        '**/src/{components,layouts,pages}/**/*.[j|t]s?(x)'
+        './node_modules/@newhighsco/{chipset,press-start}/**/*.[j|t]s?(x)',
+        './src/{components,layouts,pages}/**/*.[j|t]s?(x)'
       ],
       safelist: [/^:(global|export)/, 'html', 'body']
     }
