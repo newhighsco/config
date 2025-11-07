88053a0e93d7e7bf7b95a51c449e8c9ed3f51e30
Ben Edwards <hello@benedfit.com>
2025-11-07 15:22:28 +0000

    feat(eslint): further migration to flat config

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -7,95 +7,114 @@ import json from '@eslint/json'
 import { defineConfig } from 'eslint/config'
 import cypress from 'eslint-plugin-cypress'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import testingLibrary from 'eslint-plugin-testing-library'
+import globals from 'globals'
 
-const gitignore = join(process.cwd(), '.gitignore')
-const compat = new FlatCompat()
-const ignore = existsSync(gitignore)
+const existsDependency = async name =>
+  await import(name).then(() => true).catch(() => false)
 
-const hasDependency = async dependency =>
-  await import(dependency).then(() => true).catch(() => false)
+const optionalDependency = async name =>
+  await import(name)
+    .then(({ default: plugin }) => plugin)
+    .catch(() => undefined)
 
-export default defineConfig(
-  [
-    ...compat.config({
-      parserOptions: { requireConfigFile: false },
-      env: { jest: true },
-      ignorePatterns: ['!.github', '!.storybook'],
-      plugins: ['simple-import-sort'],
-      rules: {
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error'
-      },
-      overrides: [
-        // Javascript
-        {
-          files: ['*.?(c|m)js'],
-          extends: ['standard', 'plugin:prettier/recommended']
-        },
-        // Typescript
-        (await hasDependency('typescript')) && {
-          files: ['*.ts?(x)'],
-          extends: ['love', 'plugin:prettier/recommended'],
-          parserOptions: {
-            project: './tsconfig.json',
-            warnOnUnsupportedTypeScriptVersion: false
-          },
-          rules: {
-            '@typescript-eslint/consistent-type-assertions': [
-              'error',
-              { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow' }
-            ],
-            '@typescript-eslint/no-unsafe-argument': 'off',
-            '@typescript-eslint/prefer-nullish-coalescing': 'off',
-            '@typescript-eslint/strict-boolean-expressions': 'off'
-          }
-        },
-        // React
-        {
-          files: ['*.{js,ts,md}x'],
-          env: { browser: true },
-          extends: [
-            'standard-react',
-            'standard-jsx',
-            'plugin:prettier/recommended',
-            'plugin:jsx-a11y/recommended',
-            'plugin:mdx/recommended'
-          ],
-          rules: {
-            // Overrides standard-react
-            'jsx-quotes': ['error', 'prefer-double']
-          }
-        },
-        // Storybook
-        (await hasDependency('storybook')) && {
-          files: ['*.stories.*'],
-          extends: [
-            'plugin:storybook/recommended',
-            'plugin:storybook/csf-strict'
-          ],
-          rules: { 'storybook/meta-inline-properties': 'error' }
-        }
-      ].filter(Boolean)
-    }),
-    // JSON
-    { files: ['**/*.json'], plugins: { json }, language: 'json/json' },
-    { files: ['**/*.json'], ...prettierRecommended },
-    // Testing Library
+const legacy = new FlatCompat().config({
+  overrides: [
+    // Javascript
     {
-      files: ['**/*.spec.*'],
-      ignores: ['**/*.spec.?(s)css'],
-      ...testingLibrary.configs['flat/react'],
+      files: ['*.?(c|m)js'],
+      extends: ['standard', 'plugin:prettier/recommended']
+    },
+    // Typescript
+    (await existsDependency('typescript')) && {
+      files: ['*.ts?(x)'],
+      extends: ['love', 'plugin:prettier/recommended'],
+      parserOptions: {
+        project: './tsconfig.json',
+        warnOnUnsupportedTypeScriptVersion: false
+      },
       rules: {
-        ...testingLibrary.configs['flat/react'].rules,
-        'testing-library/no-node-access': [
+        '@typescript-eslint/consistent-type-assertions': [
           'error',
-          { allowContainerFirstChild: true }
-        ]
+          { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow' }
+        ],
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off'
       }
     },
-    // Cypress
-    { files: ['**/*.cy.*'], extends: [cypress.configs.recommended] },
-    ignore && includeIgnoreFile(gitignore)
+    // React
+    {
+      files: ['*.{js,ts,md}x'],
+      env: { browser: true },
+      extends: [
+        'standard-react',
+        'standard-jsx',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:mdx/recommended'
+      ],
+      rules: {
+        // Overrides standard-react
+        'jsx-quotes': ['error', 'prefer-double']
+      }
+    }
   ].filter(Boolean)
-)
+})
+
+// console.log(111, JSON.stringify(legacy, null, 2))
+
+const configs = [
+  {
+    ignores: ['!.github', '!.storybook'],
+    languageOptions: { globals: { ...globals.jest } },
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  },
+  ...legacy,
+  // JSON
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json' },
+  { files: ['**/*.json'], ...prettierRecommended },
+  // Testing Library
+  {
+    files: ['**/*.spec.*'],
+    ignores: ['**/*.spec.?(s)css'],
+    ...testingLibrary.configs['flat/react'],
+    rules: {
+      ...testingLibrary.configs['flat/react'].rules,
+      'testing-library/no-node-access': [
+        'error',
+        { allowContainerFirstChild: true }
+      ]
+    }
+  },
+  // Cypress
+  { files: ['**/*.cy.*'], extends: [cypress.configs.recommended] }
+]
+
+// Storybook
+if (await existsDependency('storybook')) {
+  const storybook = await optionalDependency('eslint-plugin-storybook')
+
+  configs.push(
+    ...storybook.configs['flat/recommended'],
+    ...storybook.configs['flat/csf-strict'],
+    {
+      files: ['**/*.stories.*'],
+      rules: { 'storybook/meta-inline-properties': 'error' }
+    }
+  )
+}
+
+// .gitignore
+const gitignore = join(process.cwd(), '.gitignore')
+
+if (existsSync(gitignore)) {
+  configs.push(includeIgnoreFile(gitignore))
+}
+
+export default defineConfig(configs)
