8c6793910797fd986e2a8f098b345c57a21f6957
Ben Edwards <hello@benedfit.com>
2026-05-26 11:20:07 +0100

    feat(release-config): find breaking changes in type/scope

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,27 +1,7 @@
-module.exports = {
-  rules: {
-    'header-max-length': [0, 'always', 72],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test'
-      ]
-    ]
-  }
+import { RuleConfigSeverity } from '@commitlint/types'
+
+/** @type {import("@commitlint/types").UserConfig} */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: { 'header-max-length': [RuleConfigSeverity.Error, 'always', 72] }
 }
